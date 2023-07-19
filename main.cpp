e26d2c467aa56f6f6d93c74823a227de111f1df8
hsspr-vfsa <139246292+hsspr-vfsa@users.noreply.github.com>
2023-07-19 08:25:40 +0000

    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World from main branch !" << std::endl;
+  int a = 0;
+  std::cout << "Hello World from main branch on codespaces !" << std::endl;
+  a = 1;
   return 0;
 }
